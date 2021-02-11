d11b18d37a42afa56950a034176e50f937963868
Alejandro <alejobecerra1995@gmail.com>
2021-02-10 22:51:01 -0500

    added 6 new exercises

--- if-statement.js
+++ if-statement.js
@@ -5,5 +5,5 @@ if (fruit.length > 5){
     console.log("The fruit name has more than five characters.");
 }
 else {
-    console.log("The fruit name has five characters or less.");
+    console.log("The fruit name has five characters or less.    ");
 }
